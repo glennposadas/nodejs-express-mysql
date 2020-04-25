77916dab66bb715dd8232db572c0554baa6eb694
Glenn Posadas <hello@glennvon.com>
2020-04-25 20:04:35 +0800

    fix sequelize by adding sequelizer model.

--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,27 @@
-const mysql = require("mysql2");
 const dbConfig = require("../config/db.config.js");
 
-var connection = mysql.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+const customerModelSequelizer = require("./customer.model.sequelizer")
 
-module.exports = connection;
+const Sequelize = require("sequelize")
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: 0,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+})
+
+const db = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.customer = customerModelSequelizer(sequelize, Sequelize)
+
+module.exports = db
