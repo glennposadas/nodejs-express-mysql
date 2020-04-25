31ad2be434d5639436241f3c252e29cc2fd77fd8
Glenn Posadas <hello@glennvon.com>
2020-04-26 01:13:10 +0800

    add morgan.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const morgan = require("morgan")
 
 const app = express()
 
@@ -8,7 +9,11 @@ var corsOptions = {
   origin: "http://localhost:8081"
 }
 
+// cors
 app.use(cors(corsOptions))
+
+// morgan
+app.use(morgan("combined"))
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
@@ -21,17 +26,20 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." })
 });
 
-require("./app/routes/customer.routes.js")(app)
+require("./app/routes/customer.routes")(app)
 
 const db = require("./app/models/db")
 
+/*
 if (process.env.NODE_ENV === 'production') {
   db.sequelize.sync()
 } else {
   db.sequelize.sync({ force : true}).then(() => {
     console.log("Drop and re-sync db.")
   })
-}
+}*/
+
+db.sequelize.sync()
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
