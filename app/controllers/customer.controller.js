be9dbeaa5eea3e3da779f4ad43d5cae000f10b87
Glenn Posadas <hello@glennvon.com>
2020-04-26 01:14:10 +0800

    update syntaxes to sequelize.

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -6,66 +6,103 @@
  * 
  */
 
-const Customer = require("../models/customer.model");
+const db = require("../models/db")
+const Customer = db.customer
+const Op = db.Sequelize.Op
 
 // Create and Save a new Customer
-module.exports.create = (req, res) => {
+exports.create = (req, res) => {
   // Validate request
   if (!req.body) {
     res.status(400).send({
       message: "Content can not be empty!"
-    });
+    })
   }
 
   // Create a Customer
-  const customer = new Customer({
+  const customer = {
     email: req.body.email,
     name: req.body.name,
     active: req.body.active
-  });
+  }
 
   // Save Customer in the database
-  Customer.create(customer, (err, data) => {
-    if (err)
+  Customer.create(customer)
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Customer."
-      });
-    else res.send(data);
-  });
-};
+          err.message || "An error has occured while creating the customer."
+      })
+    })
+}
 
 // Retrieve all Customers from the database.
-module.exports.findAll = (req, res) => {
-  Customer.getAll((err, data) => {
-    if (err)
+exports.findAll = (req, res) => {
+
+  console.log("Find all customers")
+
+  Customer.findAll()
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while retrieving customers."
-      });
-    else res.send(data);
-  });
+      })
+    })
 };
 
 // Find a single Customer with a customerId
-module.exports.findOne = (req, res) => {
-  Customer.findById(req.params.customerId, (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found Customer with id ${req.params.customerId}.`
-        });
-      } else {
-        res.status(500).send({
-          message: "Error retrieving Customer with id " + req.params.customerId
-        });
-      }
-    } else res.send(data);
-  });
-};
+exports.findOne = (req, res) => {
+  const customerId = req.params.customerId
+
+  console.log("Find by PK: " + customerId)
+
+  Customer.findByPk(customerId)
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Error while finding a customer by ID"
+      })
+    })
+}
+
+// search for customer by email or name.
+exports.search = (req, res) => {
+  const q = req.query.q
+
+  // Validate request
+  if (!q) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    })
+  }
+  
+  console.log("Search for: " + q)
+
+  const condition = q ? { name: { [Op.like]: `%${q}%` } } : null
+
+  Customer.findAll({ where: condition })
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while searching for customers."
+      })
+    })
+}
 
 // Update a Customer identified by the customerId in the request
-module.exports.update = (req, res) => {
+exports.update = (req, res) => {
   // Validate Request
   if (!req.body) {
     res.status(400).send({
@@ -75,27 +112,31 @@ module.exports.update = (req, res) => {
 
   console.log(req.body);
 
-  Customer.updateById(
-    req.params.customerId,
-    new Customer(req.body),
-    (err, data) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          res.status(404).send({
-            message: `Not found Customer with id ${req.params.customerId}.`
-          });
-        } else {
-          res.status(500).send({
-            message: "Error updating Customer with id " + req.params.customerId
-          });
-        }
-      } else res.send(data);
-    }
-  );
-};
+  const customerId = req.params.customerId
+
+  Customer.update(req.body, {
+    where: { id: customerId }
+  })
+    .then(num => {
+      if(num == 1) {
+        res.send ({
+          message: "Customer record has been updated successfully!"
+        })
+      } else {
+        res.send({
+          message: "Customer ${customerId} couldn't be updated. Please try again later!."
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Customer with id " + req.params.customerId
+      })
+    })
+}
 
 // Delete a Customer with the specified customerId in the request
-module.exports.delete = (req, res) => {
+exports.delete = (req, res) => {
   Customer.remove(req.params.customerId, (err, data) => {
     if (err) {
       if (err.kind === "not_found") {
@@ -112,7 +153,7 @@ module.exports.delete = (req, res) => {
 };
 
 // Delete all Customers from the database.
-module.exports.deleteAll = (req, res) => {
+exports.deleteAll = (req, res) => {
   Customer.removeAll((err, data) => {
     if (err)
       res.status(500).send({
@@ -121,4 +162,4 @@ module.exports.deleteAll = (req, res) => {
       });
     else res.send({ message: `All Customers were deleted successfully!` });
   });
-};
+}
